76321a67513d2461d1982ff2a6701a9d35f62409
zengDeveloper <602275859@qq.com>
2017-11-15 16:23:19 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login-box" id="app" >  
+ <el-row>  
+    <el-col :span="8">  
+        <el-input id="name"  v-model="username" placeholder="请输入帐号">  
+            <template slot="prepend">账户</template>
+        </el-input>   
+    </el-col>  
+ </el-row>  
+ <el-row>  
+    <el-col :span="8">  
+        <el-input id="password" v-model="password" type="password" placeholder="请输入密码">  
+            <template slot="prepend">密码</template>  
+        </el-input>  
+    </el-col>  
+ </el-row>  
+ <el-row>  
+    <el-col :span="8">  
+        <el-button id="login" v-on:click="check" style="width:100%" type="primary">登录</el-button>  
+    </el-col>  
+ </el-row>  
+</div>   
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      username:"",
+      password:""
+    }
+  },
+  methods:{
+    check:function(){
+      this.$http.post('/shirospring/login',{
+        username:this.username,
+        password:this.password
+      }).then(function(res){
+        alert(JSON.stringify(res))
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.login-box{
+  margin-left: 30%;
+  margin-top: 20%;
+}
+.el-col{
+  margin-top: 10px;
+}
+
+</style>
