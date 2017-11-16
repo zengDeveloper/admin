93313dc475b2873476fd8a69cd991a5cfc9acdd9
zengDeveloper <602275859@qq.com>
2017-11-16 09:33:46 +0800

    login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="login-box" id="app" >  
  <el-row>  
-    <el-col :span="8">  
-        <el-input id="name"  v-model="username" placeholder="请输入帐号">  
+    <el-col :span="8">
+        <el-input id="name" name="username"  v-model="username" placeholder="请输入帐号">  
             <template slot="prepend">账户</template>
         </el-input>   
     </el-col>  
  </el-row>  
  <el-row>  
     <el-col :span="8">  
-        <el-input id="password" v-model="password" type="password" placeholder="请输入密码">  
+        <el-input id="password" name="password" v-model="password" type="password" placeholder="请输入密码">  
             <template slot="prepend">密码</template>  
         </el-input>  
     </el-col>  
  </el-row>  
  <el-row>  
     <el-col :span="8">  
-        <el-button id="login" v-on:click="check" style="width:100%" type="primary">登录</el-button>  
+        <el-button @click="check" type="primary">登录</el-button>
     </el-col>  
  </el-row>  
 </div>   
@@ -34,7 +34,8 @@ export default {
   },
   methods:{
     check:function(){
-      this.$http.post('/shirospring/login',{
+      console.log(this.username)
+      this.$http.post('/shirospring/loginWeb',{
         username:this.username,
         password:this.password
       }).then(function(res){
