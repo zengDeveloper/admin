d50f0aa705af172cbb309bcda9d72953a0a4d77c
xiangzeng <190093037@qq.com>
2017-11-16 18:09:17 +0800

    '前端菜单'

---
+++ src/pages/common/hearder.vue
@@ -0,0 +1,3 @@
+<template>
+    <el-row :span="2">3</el-row>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    
+    <v-menu></v-menu>
+</template>
+<script>
+import menu from './common/menu'
+    export default{
+        components:{menu}
+    }
+</script>
