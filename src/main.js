c1d33010a1122442812ea8d88c716bc377ed3fef
xiangzeng <190093037@qq.com>
2017-11-21 18:06:23 +0800

    '进度条'

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import 'nprogress/nprogress.css'
+
 Vue.config.productionTip = false
 Vue.use(Element)
 Vue.use(VueAxios, axios)
